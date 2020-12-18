85f631de7424e7620d1a3d6c61511d1121eb0bad
mbenzekri <benzekri.mohamed@gmail.com>
2020-12-18 01:42:41 +0100

    fix barcode

--- grab-frame-take-photo.js
+++ grab-frame-take-photo.js
@@ -86,7 +86,7 @@ function onBarcodeClick() {
         interval = setInterval(_ => {
             imageCapture.grabFrame()
                 .then(imageBitmap => detector.detect(imageBitmap))
-                .then(code => detected.value = code)
+                .then(barcode => detected.value = barcode.rawData)
                 .catch(err => document.querySelector('#framediv').innerHTML = err.message);
         },500)
     }
